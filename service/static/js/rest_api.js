571b8d6d2d9cbc7c2136ac2b8637d267013bf930
LianghaoShu <shulh8@gmail.com>
2021-08-03 17:26:41 -0700

    modified retrieve button not finished

--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -113,30 +113,85 @@ $(function () {
   $("#retrieve-btn").click(function () {
     var shopcart_id = $("#shopcart_customer_id").val();
     var product_id = $("#shopcart_product_id").val();
+    var queryString = "";
+    alert(queryString);
+    if(shopcart_id){
+        queryString+= "/shopcarts?"+"shopcart_id="+shopcart_id;
+    }
 
-    if (product_id) {
-      var ajax = $.ajax({
-        type: "GET",
-        url: `/shopcarts/${shopcart_id}/items/${product_id}`,
-        contentType: "application/json",
-        data: "",
-      });
+    if(product_id) {
+        queryString= "/shopcarts/"+shopcart_id+"/items/" + product_id;
+    }
 
+
+    var ajax = $.ajax({
+      type: "GET",
+      url: queryString  ,
+      contentType: "application/json",
+      data: "",
+    });
+
+
+    alert("retrieve");
+    if(product_id){
       ajax.done(function (res) {
-        //alert (JSON.stringify(res))
+          //alert (JSON.stringify(res))
         update_form_data(res);
         flash_message("Success");
         $("#flash_message").attr("class", "text-success");
       });
-      ajax.fail(function (res) {
-        clear_form_data();
-        flash_message(res.responseJSON.message);
-        $("#flash_message").attr("class", "text-danger");
-      });
-    } else {
-      flash_message("Missing: product_id");
-      $("#flash_message").attr("class", "text-danger");
+      
     }
+    else{
+      ajax.done(function (res) {
+        //alert(res.toSource())
+        $("#search_results").empty();
+        var header = `
+          <table class="table-dark w-100" cellpadding="3">
+            <thead>
+              <tr>
+                <th class="col-12 col-md-2 text-center">Customer ID</th>
+                <th class="col-12 col-md-3 text-center">Product ID</th>
+                <th class="col-12 col-md-4 text-center">Quantity</th>
+                <th class="col-12 col-md-3 text-center">Price</th>
+                <th class="col-12 col-md-3 text-center">Checkout</th>
+              </tr>
+            </thead>
+        `;
+  
+        var firstItem = "";
+        var rows = res.map((item) => {
+          // copy the first result to the form
+          if (!firstItem) update_form_data(item);
+          // build table
+          return `<tr><td class="text-center">
+            ${item.shopcart_id}
+            </td><td class="text-center">
+            ${item.product_id}
+            </td><td class="text-center">
+            ${item.quantity}
+            </td><td class="text-center">
+            ${item.price}
+            </td><td class="text-center">
+            ${item.checkout}
+            </td></tr>`;
+        });
+  
+        var table = header.concat(...rows, "</table>");
+  
+        $("#search_results").append(table);
+  
+        flash_message("Success");
+        $("#flash_message").attr("class", "text-success");
+      });
+    }
+
+     
+    ajax.fail(function (res) {
+      clear_form_data();
+      flash_message(res.responseJSON.message);
+      $("#flash_message").attr("class", "text-danger");
+    });
   });
 
   // ****************************************
@@ -155,7 +210,7 @@ $(function () {
       queryString+="/items/"+product_id
     }
 
-    //alert(queryString)
+    
     var ajax = $.ajax({
       type: "DELETE",
       url: `/shopcarts/`+queryString,
